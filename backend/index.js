997eebac8905ad41d8f03cba42df5fd63856ff32
Flukena <flukgoza@gmail.com>
2021-04-25 14:11:17 +0700

    start

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path')
+const moment = require('moment')
 const app = express();
 
 
+const logger = (req, res, next)=>{
+    console.log(`${req.protocol}://${req.get('host')} ${req.originalUrl}: ${moment().format()}`)
+}
+app.use(logger)
 
+app.use(express.static('static'))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const PORT  = process.env.PORT || 3000
 
